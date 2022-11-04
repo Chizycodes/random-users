f1ae20a8672ff6e45a529ce00509995211a29494
Joy Okwudire <okwudirejoy@gmail.com>
2022-11-04 11:20:02 +0000

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import UsersList from './pages/usersList';
-import User from './pages/user';
 import Home from './pages/home';
 import Error404 from './pages/error404';
 import Login from './pages/login';
